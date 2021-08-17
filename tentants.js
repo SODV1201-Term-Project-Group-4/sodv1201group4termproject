978a0da0bca5938d6e0904f76cd34efc57932d9c
Paul K Kho <paulkckho@shaw.ca>
2021-08-17 15:54:16 -0600

    Revisions
    These are revisions to our website. We must adjust and modified our files accordingly to given instructors by our instructor.

---
+++ tentants.js
@@ -0,0 +1,6 @@
+// SODV 1201 Term Project JavaScript page 1 
+//   Programmed by SODV 1201 Group 4, Paul K Kho and Muhammad Haider
+//   Based on instructions provided by Dima Marachi, SODV 1201 instructor
+//   Course name: Introduction to Web Programming
+//   Course number: SODV 1201 
+//   Due: August 16 2021
---
+++ termprojectJavaScript.js
@@ -0,0 +1,10 @@
+// SODV 1201 Term Project JavaScript page 1 
+//   Programmed by SODV 1201 Group 4, Paul K Kho and Muhammad Haider
+//   Based on instructions provided by Dima Marachi, SODV 1201 instructor
+//   Course name: Introduction to Web Programming
+//   Course number: SODV 1201 
+//   Due: August 16 2021
+
+function RegArchive1() {
+    alert("Data entered sucessfully. Thank you.");
+}
